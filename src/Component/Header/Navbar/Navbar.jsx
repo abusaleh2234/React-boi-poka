c2e7d075fc0473ffa835ca8febcd4ba6bce157d5
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-04-25 16:09:23 +0600

    add header footer and fake data

--- src/Component/Header/Navbar/Navbar.jsx
+++ src/Component/Header/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
-
+    const menu = <>
+        <li className=''><NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending " : isActive ? "font-semibold text-[#23BE0A] border rounded-md border-[#23BE0A]" : ""
+            }
+        >
+            Home
+        </NavLink></li>
+        <li><NavLink
+            to="/messages"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+            Listed Books
+        </NavLink></li>
+        <li><NavLink
+            to="/messages"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+            Pages To Read
+        </NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -12,33 +37,23 @@ const Navbar = () => {
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            menu
+                        }
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Book Vibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {
+                        menu
+                    }
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+            <div className="navbar-end space-x-3">
+                <a className="btn bg-[#23BE0A] text-white">Sign in</a>
+                <a className="btn bg-[#59C6D2] text-white">Sign Up</a>
             </div>
         </div>
     );
