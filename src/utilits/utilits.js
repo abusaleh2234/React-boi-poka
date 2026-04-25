3638fbbae81c1ea6fe5aadfb63aa3a2d38572f42
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-04-25 21:20:00 +0600

    read list add local storage

---
+++ src/utilits/utilits.js
@@ -0,0 +1,26 @@
+const getStoredBook = () => {
+    const storedBookStr = localStorage.getItem("readList")
+
+    if (storedBookStr) {
+        const storedBookData = JSON.parse(storedBookStr)
+        return storedBookData
+    }
+    else {
+        return []
+    }
+}
+
+const addStoredBook = (id) => {
+    const storedBook = getStoredBook()
+
+    if (storedBook.includes(id)) {
+        alert("This book is already exist")
+    }
+    else {
+        storedBook.push(id)
+        const data = JSON.stringify(storedBook)
+        localStorage.setItem("readList", data)
+    }
+}
+
+export { addStoredBook }
