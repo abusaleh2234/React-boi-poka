f560bb15e418d686ce955de66f22f02758239690
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-04-25 12:21:47 +0600

    project setup complete

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import Navbar from "../../Component/Header/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
